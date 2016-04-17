6c509cbc07bf797a80729874b7da5887b3218a91
Martin Hochel <hochelmartin@gmail.com>
2016-04-17 21:45:03 +0200

    test(ts-helpers): add basic coverage that helpers are correctly created on global

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
+import './index';
 
 describe( `ts-helpers`, () => {
+
+  const hasOwnProperty = Object.prototype.hasOwnProperty;
 
   it( `should work`, () => {
 
@@ -8,5 +11,46 @@ describe( `ts-helpers`, () => {
 
   } );
 
+  describe( `__assign`, () => {
+
+    it( `should exist on global`, () => {
+
+      expect( hasOwnProperty.call( global, '__assign' ) ).to.equal( true );
+
+    } );
+
+  } );
+
+  describe( `__extends`, () => {
+
+    it( `should exists on global`, () => {
+
+      expect( hasOwnProperty.call( global, '__extends' ) ).to.equal( true );
+
+    } );
+
+  } );
+
+  describe( `__awaiter`, () => {
+
+    it( `should exists on global`, () => {
+
+      expect( hasOwnProperty.call( global, '__awaiter' ) ).to.equal( true );
+
+    } );
+
+  } );
+
+  describe( `__decorate, __param, __metadata`, () => {
+
+    it( `should exists on global`, () => {
+
+      expect( hasOwnProperty.call( global, '__decorate' ) ).to.equal( true );
+      expect( hasOwnProperty.call( global, '__param' ) ).to.equal( true );
+      expect( hasOwnProperty.call( global, '__metadata' ) ).to.equal( true );
+
+    } );
+
+  } );
 
 } );
